efb7dd4e7f7345f8f438524f6ac6dbde843e4db7
Shakil Ahmed <shakil.devcraft@gmail.com>
2026-04-20 16:30:05 +0600

    first commit

---
+++ src/app/hero/page.jsx
@@ -0,0 +1,14 @@
+import { Button } from "@heroui/react";
+
+const HeroPage = () => {
+    return (
+        <div>
+            <h2>Hero Page</h2>
+            <Button>Free Designs</Button>
+            <Button variant="secondary">Free Designs</Button>
+            <Button variant="danger">Free Designs</Button>
+        </div>
+    );
+};
+
+export default HeroPage;
---
+++ src/app/tasks/page.jsx
@@ -0,0 +1,10 @@
+
+const TasksPage = () => {
+    return (
+        <div>
+            <h2>Tasks: 0</h2>            
+        </div>
+    );
+};
+
+export default TasksPage;
